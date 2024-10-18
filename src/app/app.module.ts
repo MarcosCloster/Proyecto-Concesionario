bdc042c1a4b16fc51cb4f3ee8f9b59c2aa4a529f
MarcosCloster <marcoscloster04@gmail.com>
2024-10-18 19:39:45 -0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './otherComponents/header/header.component';
+import { FooterComponent } from './otherComponents/footer/footer.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ShopComponent } from './shop/shop.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainPageComponent,
+    ShopComponent,
+    AboutUsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
