642f9b0d29eee8aa93960fb6247f425a87075f6d
MarcosCloster <marcoscloster04@gmail.com>
2024-11-01 20:03:49 -0300

    Update main page with the api

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FooterComponent } from '../otherComponents/footer/footer.component';
 import { HeaderComponent } from '../otherComponents/header/header.component';
+import { JsonService } from '../services/json.service';
+import { Auto } from '../interfaces/autos';
 
 @Component({
   selector: 'app-main-page',
@@ -9,6 +11,29 @@ import { HeaderComponent } from '../otherComponents/header/header.component';
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit{
+  sv = inject(JsonService)
+  highlightCar: Auto[] = []
 
+  ngOnInit(): void {
+    this.getAutos()
+  }
+
+  getAutos(){
+    this.sv.getJson().subscribe({
+      next: (auto: Auto[]) => {
+        auto.reverse();
+        let i = 0;
+        while(i != 5)
+        {
+          this.highlightCar.push(auto[i])
+          i++
+        }
+        console.log(this.highlightCar)
+      },
+      error: (error: Error) => {
+        console.log(error)
+      }
+    })
+  }
 }
