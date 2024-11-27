600e3bdbb7e430f8cc1a04092145145176d20ac4
inaki2004 <akitomas7600@gmail.com>
2024-11-27 17:24:47 -0300

    completed view-date component

--- src/app/admin/view-date/view-date.component.ts
+++ src/app/admin/view-date/view-date.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { NotExpr } from '@angular/compiler';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Reserva } from 'src/app/interfaces/reserva';
+import { DateService } from 'src/app/services/date.service';
 
 @Component({
   selector: 'app-view-date',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './view-date.component.html',
   styleUrl: './view-date.component.css'
 })
-export class ViewDateComponent {
+export class ViewDateComponent implements OnInit{
+  ngOnInit(): void {
+    this.getReservation()
+  }
 
+  arrayReservation!: Reserva[];
+  reservationService = inject(DateService)
+
+  getReservation()
+  {
+    this.reservationService.getReservation().subscribe({
+      next: (reservas) =>
+      {
+        this.arrayReservation = reservas
+      },
+      error: (e: Error) =>
+      {
+        console.log(e.message)
+      }
+    })
+  }
 }
