0f00c50e1a6ce29d2af360f58c9f439006992d8a
matias <matiascoluccio@gmail.com>
2024-12-04 11:26:12 -0300

    add search bar in reservations

--- src/app/admin/view-date/view-date.component.ts
+++ src/app/admin/view-date/view-date.component.ts
@@ -1,5 +1,7 @@
+import { CommonModule } from '@angular/common';
 import { NotExpr } from '@angular/compiler';
 import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { Reserva } from 'src/app/interfaces/reserva';
 import { DateService } from 'src/app/services/date.service';
@@ -7,29 +9,41 @@ import { DateService } from 'src/app/services/date.service';
 @Component({
   selector: 'app-view-date',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,FormsModule,CommonModule],
   templateUrl: './view-date.component.html',
   styleUrl: './view-date.component.css'
 })
-export class ViewDateComponent implements OnInit{
+export class ViewDateComponent implements OnInit {
   ngOnInit(): void {
-    this.getReservation()
+    this.getReservation();
   }
 
-  arrayReservation!: Reserva[];
-  reservationService = inject(DateService)
+  arrayReservation: Reserva[] = [];
+  filteredReservations: Reserva[] = []; // Lista filtrada para mostrar en la tabla
+  filterText: string = ''; // Texto ingresado en la barra de búsqueda
+  reservationService = inject(DateService);
 
-  getReservation()
-  {
+  getReservation() {
     this.reservationService.getReservation().subscribe({
-      next: (reservas) =>
-      {
-        this.arrayReservation = reservas
+      next: (reservas) => {
+        this.arrayReservation = reservas;
+        this.filteredReservations = reservas; // Inicialmente, mostramos todas las reservas
       },
-      error: (e: Error) =>
-      {
-        console.log(e.message)
+      error: (e: Error) => {
+        console.log(e.message);
       }
-    })
+    });
+  }
+
+  filterReservations() {
+    const filter = this.filterText.toLowerCase();
+    this.filteredReservations = this.arrayReservation.filter(reserva =>
+      reserva.nombre.toLowerCase().includes(filter) || // Filtra por nombre
+      reserva.email.toLowerCase().includes(filter) || // Filtra por email
+      reserva.id!.toString().includes(filter) || // Filtra por id
+      reserva.idAuto.toString().includes(filter) ||
+      reserva.fecha.toString().includes(filter) // Filtra por id de auto 
+      
+    );
   }
 }
